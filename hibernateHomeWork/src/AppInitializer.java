d7bc1e18ec501cb61de547a5af671b80752dd5ff
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-06-16 11:36:09 +0530

    Create Crud Operations for Order Entity

--- hibernateHomeWork/src/AppInitializer.java
+++ hibernateHomeWork/src/AppInitializer.java
@@ -1,8 +1,11 @@
 import lk.ijse.hibernate.entity.Customer;
 import lk.ijse.hibernate.entity.Item;
+import lk.ijse.hibernate.entity.Order;
 import lk.ijse.hibernate.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.ArrayList;
 
 public class AppInitializer {
     public static void main(String[] args) {
@@ -17,7 +20,34 @@ public class AppInitializer {
         c2.setId("C002");
         c2.setName("Rafeek");
         c2.setAddress("Panadura");
+//===========================
+        Order O1 = new Order();
+        O1.setId("O001");
+        O1.setCustomer(c1);
 
+        Order O2 = new Order();
+        O2.setId("O002");
+        O2.setCustomer(c1);
+
+        ArrayList<Order> orderArrayList = new ArrayList();
+        orderArrayList.add(O1);
+        orderArrayList.add(O2);
+
+        c1.setOrderList(orderArrayList);
+
+//===========================
+        Item I1 = new Item();
+        I1.setItemCode("I001");
+        I1.setDescription("Fresh milk");
+        I1.setUnitPrice(120);
+        I1.setQtyOnHand(100);
+
+        Item I2 = new Item();
+        I1.setItemCode("I002");
+        I1.setDescription("Samba Rice 10Kg");
+        I1.setUnitPrice(350);
+        I1.setQtyOnHand(50);
+//===========================
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
@@ -25,17 +55,35 @@ public class AppInitializer {
         session.save(c1);
         session.save(c2);
 
+        session.save(I1);
+        session.save(I2);
+
+        session.save(O1);
+        session.save(O2);
+//===========================
         Customer c001 = session.get(Customer.class, "C001");
         Customer c002 = session.get(Customer.class, "C002");
+
+        Order o001 = session.get(Order.class, "O001");
+        Order o002 = session.get(Order.class, "O002");
 
         System.out.println(c001);
         System.out.println(c002);
 
+        System.out.println(o001);
+        System.out.println(o002);
+//===========================
         c1.setAddress("meepe");
         session.update(c1);
 
+        O1.setCustomer(c2);
+        session.update(c2);
+//===========================
         Customer c003 = session.get(Customer.class,"C002");
         session.delete(c003);
+
+        Order o1 = session.get(Order.class,"O001");
+        session.delete(o1);
 
         transaction.commit();
         session.close();
