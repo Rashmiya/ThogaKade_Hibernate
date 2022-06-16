09de855e50576ea5be302f27af29eeec5e1c0cbb
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-06-16 11:05:32 +0530

    Create Item and OrderDetails class with relations

--- hibernateHomeWork/src/lk/ijse/hibernate/entity/Item.java
+++ hibernateHomeWork/src/lk/ijse/hibernate/entity/Item.java
@@ -1,6 +1,83 @@
 package lk.ijse.hibernate.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
 public class Item {
+    @Id
+    private String ItemCode;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private double unitPrice;
+    private int qtyOnHand;
 
+    @OneToMany(mappedBy = "oderDetail_item")
+    private List<OrderDetail> orderDetailsList = new ArrayList<>();
+
+    public Item() {
+    }
+
+    public Item(String itemCode, String description, double unitPrice, int qtyOnHand, List<OrderDetail> orderDetailsList) {
+        ItemCode = itemCode;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.qtyOnHand = qtyOnHand;
+        this.orderDetailsList = orderDetailsList;
+    }
+
+    public String getItemCode() {
+        return ItemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        ItemCode = itemCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public List<OrderDetail> getOrderDetailsList() {
+        return orderDetailsList;
+    }
+
+    public void setOrderDetailsList(List<OrderDetail> orderDetailsList) {
+        this.orderDetailsList = orderDetailsList;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "ItemCode='" + ItemCode + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", qtyOnHand=" + qtyOnHand +
+                ", orderDetailsList=" + orderDetailsList +
+                '}';
+    }
 }
--- hibernateHomeWork/src/lk/ijse/hibernate/entity/OrderDetail.java
+++ hibernateHomeWork/src/lk/ijse/hibernate/entity/OrderDetail.java
@@ -1,4 +1,79 @@
 package lk.ijse.hibernate.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class OrderDetail {
+    @Id
+    private String id;
+    @ManyToOne
+    private Order orderDetail_orders;
+    @ManyToOne
+    private Item oderDetail_item;
+    private int oderDetail_qty;
+    private double oderDetail_unitPrice;
+
+    public OrderDetail(String id, Order orderDetail_orders, Item oderDetail_item, int oderDetail_qty, double oderDetail_unitPrice) {
+        this.id = id;
+        this.orderDetail_orders = orderDetail_orders;
+        this.oderDetail_item = oderDetail_item;
+        this.oderDetail_qty = oderDetail_qty;
+        this.oderDetail_unitPrice = oderDetail_unitPrice;
+    }
+
+    public OrderDetail() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Order getOrderDetail_orders() {
+        return orderDetail_orders;
+    }
+
+    public void setOrderDetail_orders(Order orderDetail_orders) {
+        this.orderDetail_orders = orderDetail_orders;
+    }
+
+    public Item getOderDetail_item() {
+        return oderDetail_item;
+    }
+
+    public void setOderDetail_item(Item oderDetail_item) {
+        this.oderDetail_item = oderDetail_item;
+    }
+
+    public int getOderDetail_qty() {
+        return oderDetail_qty;
+    }
+
+    public void setOderDetail_qty(int oderDetail_qtyOnHand) {
+        this.oderDetail_qty = oderDetail_qtyOnHand;
+    }
+
+    public double getOderDetail_unitPrice() {
+        return oderDetail_unitPrice;
+    }
+
+    public void setOderDetail_unitPrice(double oderDetail_unitPrice) {
+        this.oderDetail_unitPrice = oderDetail_unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "id='" + id + '\'' +
+                ", orderDetail_orders=" + orderDetail_orders +
+                ", oderDetail_item=" + oderDetail_item +
+                ", oderDetail_qty=" + oderDetail_qty+
+                ", oderDetail_unitPrice=" + oderDetail_unitPrice +
+                '}';
+    }
 }
